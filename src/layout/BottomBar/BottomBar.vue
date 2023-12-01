3d9ef46434189c6ab80ffd74995fe573de209d2e
senong <1120659490@qq.com>
2023-12-01 21:57:35 +0800

    feat: add footer

--- src/layout/BottomBar/BottomBar.vue
+++ src/layout/BottomBar/BottomBar.vue
@@ -1,11 +1,34 @@
 <script lang="ts" setup>
+import { useTheme } from '@/hooks/useTheme';
+
+
+const { activeThemeName } = useTheme()
+
 
 </script>
 <template>
-  <div class="bottom-bar">
-
+  <div class="bottom-bar" absolute bottom-0 w-full flex items-center justify-start>
+    <v-card :theme="activeThemeName">
+      <v-chip variant="text">
+        <span>
+          <span font-size-2>2023 </span> - ∞
+        </span>
+      </v-chip>
+      <v-btn variant="plain" size="small" href="https://beian.miit.gov.cn/" target="_blank">@Senong</v-btn>
+      <v-btn variant="plain" size="small" href="https://beian.miit.gov.cn/" target="_blank">粤ICP备2023134767号-1</v-btn>
+    </v-card>
   </div>
 </template>
-<style lang="" scoped>
+<style lang="scss" scoped>
+.bottom-bar {
+  .v-card {
+    background-color: transparent;
+    border: 0;
+    box-shadow: none;
+  }
 
+  .v-btn {
+    background-color: transparent !important;
+  }
+}
 </style>
